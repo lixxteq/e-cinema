e66b771052ef664ff4c7721d849b8da9c24a23f7
lixxteq <rlv1zhakov.liv@gmail.com>
2023-10-28 19:32:10 +0300

    feat: local mysql server running on docker

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ from os import getenv, path
 
 class Config(object):
     SECRET_KEY = '018b681f-8947-7f04-8cc7-b2cf1ba230ef' # non-private
-    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@localhost/elib?charset=utf8&auth_plugin=mysql_native_password'
     UPLOAD_FOLDER = path.join(path.dirname(path.abspath(__file__)), 'upload')
     TEMPLATES_AUTO_RELOAD = True
 
